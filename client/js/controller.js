f276f25b4f6b2d98660e9d92ebe11547c9be379b
Andrew Yeung <andrewhyeung@gmail.com>
2016-09-20 18:46:10 -0700

    added confirmation message to form submit

--- client/js/controller.js
+++ client/js/controller.js
@@ -1,12 +1,14 @@
 myApp.controller('emailController', function($scope, emailFactory){
 
 	$scope.submitForm = function(){
+		
 		form = {
 			name: $scope.name, 
 			email: $scope.email,
 			message: $scope.messages
 		}
-		emailFactory.submitForm(form); 
+		emailFactory.submitForm(form);
+		$scope.showForm = true; 
 		$scope.name = null; 
 		$scope.email = null; 
 		$scope.messages = null; 
